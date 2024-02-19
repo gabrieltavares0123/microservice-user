dcbfad7e597ec7a2da1be85fe8b3a0170ebbca59
Gabriel Jorge <gabriel.tavaresramos@gmail.com>
2024-02-19 20:32:48 -0300

    Add rabbitMQ configuration and properties

---
+++ src/main/kotlin/com/ms/user/config/RabbitMQConfig.kt
@@ -0,0 +1,15 @@
+package com.ms.user.config
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class RabbitMQConfig {
+    @Bean
+    fun messageConverter(): Jackson2JsonMessageConverter {
+        val objectMapper = ObjectMapper()
+        return Jackson2JsonMessageConverter(objectMapper)
+    }
+}
