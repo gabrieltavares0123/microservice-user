4752f39fe175f541902e5369b1462f71a04dbe70
Gabriel Jorge <gabriel.tavaresramos@gmail.com>
2024-02-19 21:05:27 -0300

    Add name for project and configure ObjectMapper to use Kotlin data classes

--- src/main/kotlin/com/ms/user/config/RabbitMQConfig.kt
+++ src/main/kotlin/com/ms/user/config/RabbitMQConfig.kt
@@ -1,6 +1,7 @@
 package com.ms.user.config
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -10,6 +11,7 @@ class RabbitMQConfig {
     @Bean
     fun messageConverter(): Jackson2JsonMessageConverter {
         val objectMapper = ObjectMapper()
+        objectMapper.registerKotlinModule()
         return Jackson2JsonMessageConverter(objectMapper)
     }
 }
